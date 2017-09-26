d2836a824765b5b3e4446171d2d255a38229aa18
Akshay Mittal <akshaycanodia@gmail.com>
2017-09-25 20:55:57 -0500

    intial commit

---
+++ Gighub1/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Gighub1.Models;
+
+namespace Gighub1.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public int Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public  IEnumerable<Genre> Genres { get; set; }
+    }
+}
