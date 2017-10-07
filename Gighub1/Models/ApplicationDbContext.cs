9d81d787305b4215c16e2ee36f052cbbe66570bc
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-07 15:38:42 -0500

    create model for attendance

--- Gighub1/Models/ApplicationDbContext.cs
+++ Gighub1/Models/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
-﻿using System.Data.Entity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace Gighub1.Models
 {
@@ -7,6 +7,7 @@ namespace Gighub1.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -16,6 +17,16 @@ namespace Gighub1.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        //fluentAPi
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
