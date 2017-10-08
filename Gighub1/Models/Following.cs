6ff15ba52d36451db5159ce5de202115722c3f7a
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-08 17:38:07 -0500

    Implement Follower and Followee Users

---
+++ Gighub1/Models/Following.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gighub1.Models
+{
+    public class Following
+    {
+        [Key]
+        [Column(Order = 1)]
+        public string FollowerId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public string FolloweeId { get; set; }
+
+        public ApplicationUser Follower { get; set; }
+
+        public ApplicationUser Followee { get; set; }
+    }
+}
