6d01b4eff06ceb90876633784a3dac90606bebf1
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-17 21:53:13 -0500

    Implement My Upcoming Gigs and refactoring

--- Gighub1/Controllers/GigsController.cs
+++ Gighub1/Controllers/GigsController.cs
@@ -1,6 +1,7 @@
 ﻿using Gighub1.Models;
 using Gighub1.ViewModels;
 using Microsoft.AspNet.Identity;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -14,6 +15,18 @@ namespace Gighub1.Controllers
         public GigsController()
         {
             _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)
+                .ToList();
+
+            return View(gigs);
         }
 
         [Authorize]
@@ -68,7 +81,7 @@ namespace Gighub1.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
 
         }
 
